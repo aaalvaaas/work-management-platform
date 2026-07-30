05345a445b518f6f9d29daab55ece56d957effa6
Владислав Дядев <465824@niuitmo.ru>
2026-07-30 11:23:54 +0300

    feat: ✨ add mail domain model

---
+++ backend/services/mail-service/src/main/java/ru/itmo/wmp/mail/domain/MailMessage.java
@@ -0,0 +1,39 @@
+package ru.itmo.wmp.mail.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "mail_messages")
+@Getter
+@Setter
+public class MailMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String messageId;
+
+    @Column(nullable = false)
+    private String senderEmail;
+
+    @Column(nullable = false)
+    private String subject;
+
+    @Lob
+    @Column(nullable = false)
+    private String plainTextBody;
+
+    @Column(nullable = false)
+    private LocalDateTime receivedAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private MailProcessingStatus processingStatus;
+
+    private LocalDateTime processedAt;
+}
