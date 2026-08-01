68685be272d76c1918957905f4d0451a64fa7009
Владислав Дядев <465824@niuitmo.ru>
2026-08-01 12:39:33 +0300

    feat: ✨ implement unread mails fetching

--- backend/services/mail-service/src/main/java/ru/itmo/wmp/mail/infrastructure/imap/ImapMailClient.java
+++ backend/services/mail-service/src/main/java/ru/itmo/wmp/mail/infrastructure/imap/ImapMailClient.java
@@ -1,17 +1,34 @@
 package ru.itmo.wmp.mail.infrastructure.imap;
 
-import jakarta.mail.Session;
-import jakarta.mail.Store;
+import jakarta.mail.*;
+import jakarta.mail.search.FlagTerm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.itmo.wmp.mail.config.properties.MailProperties;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 @Component
 @RequiredArgsConstructor
 public class ImapMailClient {
     private final MailProperties properties;
+
+    public List<Message> getUnreadMessages() throws MessagingException {
+        Store store = connect();
+        Folder inbox = store.getFolder("INBOX");
+        inbox.open(Folder.READ_ONLY);
+
+        Message[] messages = inbox.search(
+            new FlagTerm(
+                new Flags(Flags.Flag.SEEN),
+                false
+            )
+        );
+
+        return Arrays.asList(messages);
+    }
 
     private Session createSession() {
         Properties props = new Properties();
@@ -24,12 +41,13 @@ public class ImapMailClient {
         return Session.getInstance(props);
     }
 
-    private Store connect() throws Exception {
+    private Store connect() throws MessagingException {
         Session session = createSession();
         Store store = session.getStore();
 
         store.connect(
             properties.host(),
+            properties.port(),
             properties.username(),
             properties.password()
         );
